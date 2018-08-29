cc26a90d2ac53d4f3848aa22b51bdf6b3e5fb09d
angshumanbhattacharjee <42242947+angshumanbhattacharjee@users.noreply.github.com>
2018-08-29 18:24:27 +0530

    Add files via upload

---
+++ ECommerce/src/main/java/com/model/UserPojo.java
@@ -0,0 +1,90 @@
+package com.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document
+public class UserPojo {
+	
+	@Id
+	int id;
+	String name;
+	String password;
+	String address;
+	String type;
+	String phonenumber;
+	/**
+	 * @return the id
+	 */
+	public int getId() {
+		return id;
+	}
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+	/**
+	 * @return the password
+	 */
+	public String getPassword() {
+		return password;
+	}
+	/**
+	 * @param password the password to set
+	 */
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	/**
+	 * @return the address
+	 */
+	public String getAddress() {
+		return address;
+	}
+	/**
+	 * @param address the address to set
+	 */
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	/**
+	 * @return the type
+	 */
+	public String getType() {
+		return type;
+	}
+	/**
+	 * @param type the type to set
+	 */
+	public void setType(String type) {
+		this.type = type;
+	}
+	/**
+	 * @return the phonenumber
+	 */
+	public String getPhonenumber() {
+		return phonenumber;
+	}
+	/**
+	 * @param phonenumber the phonenumber to set
+	 */
+	public void setPhonenumber(String phonenumber) {
+		this.phonenumber = phonenumber;
+	}
+	
+	
+}
